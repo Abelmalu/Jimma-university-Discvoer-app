1b1aceec958427be62d83c3ef77ef25722d2f90d
abel malu <abellmalu@gmail.com>
2025-01-17 13:40:47 +0300

    location can be displayed

--- lib/feature/cotact/ui/contact_us_screen.dart
+++ lib/feature/cotact/ui/contact_us_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-  import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class ContactUs extends StatefulWidget {
   const ContactUs({super.key});
@@ -9,19 +10,69 @@ class ContactUs extends StatefulWidget {
 }
 
 class _ContactUsState extends State<ContactUs> {
-   late GoogleMapController mapController;
-      
-        final LatLng _center = const LatLng(-33.86, 151.20);
-      
-       
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(7.6820, 36.8560);
+  final LatLng _hell = const LatLng(37.4223, -122.0848);
+  LatLng? _currentP;
+  @override
+  void initState() {
+    getLocationUpdates();
+    super.initState();
+  }
+
+  final _locationController = new Location();
+
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
-                
-                initialCameraPosition: CameraPosition(
-                  target: _center,
-                  zoom: 11.0,
-                ),
-                );
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 13.0,
+      ),
+      markers: {
+        Marker(
+            markerId: MarkerId("_destinationLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _center),
+        Marker(
+            markerId: MarkerId("_currentLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _currentP!),
+      },
+    );
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation != null && currentLocation != null) {
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          print('the current location is ${currentLocation}');
+        });
+      }
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,14 @@ import 'package:ju_discover/feature/campuses/ui/campuses_screen.dart';
 import 'package:ju_discover/feature/home/ui/tabs_screen.dart';
 import 'package:ju_discover/feature/internship/ui/Internship_screen.dart';
 import 'package:ju_discover/feature/login/ui/login_screen.dart';
+import 'package:location/location.dart';
 import 'feature/Register/bloc/register_bloc.dart';
 import 'feature/campuses/bloc/campus_bloc.dart';
 import 'feature/home/bloc/home_bloc.dart';
 import 'feature/internship/bloc/internship_bloc.dart';
 import 'feature/internship/ui/internship_detail_screen.dart';
 import 'feature/login/bloc/login_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 void main() {
@@ -72,7 +74,7 @@ class MyApp extends StatelessWidget {
         ),
         
         routes:{
-          '/':(context) => TabsScreen(),
+          '/':(context) => Demo(),
          CampusScreen.routName:(context)=>CampusScreen(),
          CampusDetailScreen.routeName:(context) => CampusDetailScreen(),
          InternshipScreen.routeName:(context) => InternshipScreen(),
@@ -88,11 +90,92 @@ class MyApp extends StatelessWidget {
 }
 
 
-class Demo extends StatelessWidget {
+class Demo extends StatefulWidget {
   const Demo({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Center(child:Text('what the hell'));
+  State<Demo> createState() => _DemoState();
+}
+
+class _DemoState extends State<Demo> {
+   late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(7.6820, 36.8560);
+  final LatLng _hell = const LatLng(37.4223, -122.0848);
+  LatLng? _currentP;
+  @override
+  void initState() {
+    getLocationUpdates();
+    super.initState();
   }
+
+  final _locationController = new Location();
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 13.0,
+      ),
+      markers: {
+        Marker(
+            markerId: MarkerId("_destinationLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _center),
+
+            if(_currentP != null)
+
+            
+        Marker(
+            markerId: MarkerId("_currentLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _currentP!),
+      
+      },
+    );
+  }
+
+ Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    // Check if the location service is enabled
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+      if (!_serviceEnabled) {
+        print('Location service not enabled');
+        return; // Exit if service is not enabled
+      }
+    }
+
+    // Check if permission is granted
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      // Request permission if not granted
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        print('Permission denied');
+        return; // Exit if permission is not granted
+      }
+    }
+
+    // Now, listen for location updates if permission and service are enabled
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation != null) {
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          print(
+              'Current location: ${currentLocation.latitude}, ${currentLocation.longitude}');
+        });
+      } else {
+        print('Received null location');
+      }
+    });
+  }
+
+
 }
